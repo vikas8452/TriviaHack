3be3dcd68488e2707ffd78ed6a332fe0c04b0e27
Vikas Patel <vikaspatel.patel4@gmail.com>
2018-05-08 00:17:31 +0530

    Fragment added instead of activity

--- app/src/main/java/com/example/dell/brainbaazihack/MainActivity.java
+++ app/src/main/java/com/example/dell/brainbaazihack/MainActivity.java
@@ -1,25 +1,18 @@
 package com.example.dell.brainbaazihack;
 
 import android.Manifest;
-import android.app.Activity;
-import android.content.ActivityNotFoundException;
-import android.content.ComponentName;
-import android.content.Intent;
+
 import android.content.pm.PackageManager;
-import android.graphics.Rect;
-import android.hardware.Camera;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.Handler;
+
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Html;
+
 import android.util.Log;
 import android.util.SparseArray;
-import android.view.MotionEvent;
-import android.view.ScaleGestureDetector;
+
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -34,17 +27,9 @@ import com.google.android.gms.vision.text.TextRecognizer;
 
 
 
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -98,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d("sdsdssd","Entered  in Oncreate");
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
+        button2.setVisibility(View.VISIBLE);
         TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
 
         if (!textRecognizer.isOperational()) {
@@ -109,22 +94,12 @@ public class MainActivity extends AppCompatActivity {
                     setFacing(CameraSource.CAMERA_FACING_BACK).
                     setAutoFocusEnabled(true).
                     setRequestedPreviewSize(1280, 1024).
-                    setRequestedFps(15.0f).
+                    setRequestedFps(8.0f).
 
 
                     build();
 
-           final Toast toast= Toast.makeText(this, "Entered", Toast.LENGTH_SHORT);
-           toast.show();
 
-
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    toast.cancel();
-                }
-            }, 300);
             surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
                 @Override
                 public void surfaceCreated(SurfaceHolder surfaceHolder) {
@@ -165,35 +140,7 @@ public class MainActivity extends AppCompatActivity {
                 {
 
 /*
-                  //  public static void main(String... args) throws Exception {
-                    URL url = null;
-                    try {
-                        url = new URL("http://stackoverflow.com/questions/2971155");
-                    } catch (MalformedURLException e) {
-                        e.printStackTrace();
-                    }
-                    BufferedReader reader = null;
-                    StringBuilder builder = new StringBuilder();
-                    try {
-                        reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
-                        for (String line; (line = reader.readLine()) != null;) {
-                            builder.append(line.trim());
-                        }
-                    } catch (UnsupportedEncodingException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } finally {
-                        if (reader != null) try { reader.close(); } catch (IOException logOrIgnore) {}
-                    }
 
-                    String start = "<div class=\"post-text\"><p>";
-                    String end = "</p>";
-                    String part = builder.substring(builder.indexOf(start) + start.length());
-                    String question = part.substring(0, part.indexOf(end));
-                    textView.setText(question);
-                  //  System.out.println(question);
-                //}
        */                textView.post(new Runnable() {
                             @Override
                             public void run() {
@@ -202,6 +149,7 @@ public class MainActivity extends AppCompatActivity {
 
                                  {
                                     TextBlock textBlock=items.valueAt(i);
+
                                     string.append(textBlock.getValue());
                                     string.append("\n");
                                  }
@@ -250,19 +198,46 @@ public class MainActivity extends AppCompatActivity {
                 String url = "http://google.co.in/search?q="+textView.getText();
              //   Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
                    //  startActivity(i);
-                Intent j=new Intent (MainActivity.this,Main2Activity.class);
-                j.putExtra("url",textView.getText());
-                startActivity(j);
+              //  Intent j=new Intent (MainActivity.this,Main2Activity.class);
+             //   j.putExtra("url",textView.getText());
+             //   startActivity(j);
+                Process fragment = new Process();
+                Bundle bundle=new Bundle();
+                bundle.putString("url", (String) textView.getText());
+                fragment.setArguments(bundle);
 
-
+            loadFragment(fragment);
+          //      button2.setVisibility(View.GONE);
             }
         });
 
     }
 
 
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        android.support.v4.app.FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        //transaction=getSupportFragmentManager().executePendingTransactions();
+        transaction.replace(R.id.container,fragment);
+          transaction.addToBackStack(null);
+        transaction.commit();
+    }
 
+    @Override
+    protected void onStart() {
+        button2.setVisibility(View.VISIBLE);
+        super.onStart();
+    }
 
+    @Override
+    protected void onRestart() {
+        button2.setVisibility(View.VISIBLE);
+        super.onRestart();
+    }
 
-
+    @Override
+    protected void onResume() {
+        button2.setVisibility(View.VISIBLE);
+        super.onResume();
+    }
 }
--- app/src/main/java/com/example/dell/brainbaazihack/Process.java
+++ app/src/main/java/com/example/dell/brainbaazihack/Process.java
@@ -4,12 +4,14 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-
+import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
@@ -19,11 +21,13 @@ import android.widget.Toast;
 import java.util.Arrays;
 import java.util.List;
 
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class Main2Activity extends AppCompatActivity {
+
+public class Process extends Fragment {
+
+
 
     @BindView(R.id.btnId)
     Button btnId;
@@ -34,45 +38,67 @@ public class Main2Activity extends AppCompatActivity {
     WebView webView1;
     @BindView(R.id.webView2)
     WebView webView2;
-    final Context myApp = this;
+    //final Context myApp = this;
     List<String> arr;
     int size;
-int count0=0;
-int count1=1;
-int count2=2;
-int optionA;
-int optionB;
-int optionC;
-String optiona;
-String optionb;
-String optionc;
-int DURATION=500;
+    int count0=0;
+    int count1=1;
+    int count2=2;
+    int optionA;
+    int optionB;
+    int optionC;
+    String optiona;
+    String optionb;
+    String optionc;
+    int DURATION=500;
+   
+    public Process() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      
+    }
+
     @SuppressLint("JavascriptInterface")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-        final WebView webView = (WebView) findViewById(R.id.webView);
-        final WebView webView1 = (WebView) findViewById(R.id.webView1);
-        final WebView webView2 = (WebView) findViewById(R.id.webView2);
-      //  StringExtractor se = new StringExtractor("http://www.google.com");
-        TextView contentView = (TextView) findViewById(R.id.contentView);
-        Intent intent = getIntent();
-        String url = intent.getStringExtra("url");
-        arr= Arrays.asList(url.split("\n"));
-        ButterKnife.bind(this);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View view= inflater.inflate(R.layout.fragment_process, container, false);
 
+        final WebView webView = (WebView) view.findViewById(R.id.webView);
+        final WebView webView1 = (WebView) view.findViewById(R.id.webView1);
+        final WebView webView2 = (WebView) view.findViewById(R.id.webView2);
+        //  StringExtractor se = new StringExtractor("http://www.google.com");
+        TextView contentView = (TextView) view.findViewById(R.id.contentView);
+
+        String url="";
+        Bundle args = getArguments();
+         url = args.getString("url");
+      //  Toast.makeText(getActivity(),url,Toast.LENGTH_SHORT).show();
+
+     //   Toast.makeText(getActivity(),url,Toast.LENGTH_SHORT).show();
+     //   Intent intent = getIntent();
+       // String url = intent.getStringExtra("url");
+        arr= Arrays.asList(url.split("\n"));
+        ButterKnife.bind(this,view);
+     //   disable.setEnabled(false);
+        btnId.setEnabled(true);
 
         /* An instance of this class will be registered as a JavaScript interface */
-      class MyJavaScriptInterface
+        class MyJavaScriptInterface
         {
             private TextView contentView;
 
             public MyJavaScriptInterface()
             {
                 Log.d("sdssdecd","Enter in Constructor");
-             //  contentView = aContentView;
-             //   Toast.makeText(Main2Activity.this, (CharSequence) contentView, Toast.LENGTH_SHORT).show();
+                //  contentView = aContentView;
+                //   Toast.makeText(getActivity().getBaseContext().this, (CharSequence) contentView, Toast.LENGTH_SHORT).show();
             }
 
 
@@ -110,7 +136,7 @@ int DURATION=500;
         webView1.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-              //  Log.i("shouldOverrideUrlLoading", url.toString());
+                //  Log.i("shouldOverrideUrlLoading", url.toString());
                 return super.shouldOverrideUrlLoading(view, url);
             }
 
@@ -128,12 +154,12 @@ int DURATION=500;
 
 
 
-    });
+        });
 
         webView2.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-              //  Log.i("shouldOverrideUrlLoading", url.toString());
+                //  Log.i("shouldOverrideUrlLoading", url.toString());
                 return super.shouldOverrideUrlLoading(view, url);
             }
 
@@ -151,7 +177,7 @@ int DURATION=500;
 
 
 
-    });
+        });
 
 
 
@@ -160,10 +186,10 @@ int DURATION=500;
 
 
 
-      //webView.findAllAsync("sc");
-       // webView.findAllAsync("Online");
+        //webView.findAllAsync("sc");
+        // webView.findAllAsync("Online");
 
-String newUrl="http://google.co.in/search?q=";
+        String newUrl="http://google.co.in/search?q=";
         for(int i=0;i<size;i++)
         {
             String s = arr.get(i);
@@ -177,7 +203,7 @@ String newUrl="http://google.co.in/search?q=";
             {
                 newUrl+=arr.get(i);
 
-             //   Toast.makeText(Main2Activity.this, newUrl, Toast.LENGTH_SHORT).show();
+                //   Toast.makeText(getActivity().getBaseContext().this, newUrl, Toast.LENGTH_SHORT).show();
             }
 
         }
@@ -195,9 +221,9 @@ String newUrl="http://google.co.in/search?q=";
             if(!(arr.get(i).contains("?")||counter>1))
             {
                 String arr1[]=arr.get(i).split(" ");
-             //   ArrayList<String> arr1=new ArrayList<arr[i].split(" ")>();
+                //   ArrayList<String> arr1=new ArrayList<arr[i].split(" ")>();
 
-               // count0=1;
+                // count0=1;
                 webView.findAllAsync(arr1[0]);
                 Log.d("dfdfdf",arr1[0]);
                 optiona=arr1[0];
@@ -214,11 +240,11 @@ String newUrl="http://google.co.in/search?q=";
         webView.setFindListener(new WebView.FindListener() {
             @Override
             public void onFindResultReceived(int i, int i1, boolean b) {
-                //  Toast.makeText(Main2Activity.this,"OPTIONS A:"+String.valueOf(i1),Toast.LENGTH_SHORT).show();
+                //  Toast.makeText(getActivity().getBaseContext().this,"OPTIONS A:"+String.valueOf(i1),Toast.LENGTH_SHORT).show();
                 optionA=i1;
                 if ( optionA > optionB && optionA > optionC ) {
 
-                    final Toast toast= Toast.makeText(Main2Activity.this, "option   A:  " + optionA + " "+optiona, Toast.LENGTH_SHORT);
+                    final Toast toast= Toast.makeText(getActivity(), "option   A:  " + optionA + " "+optiona, Toast.LENGTH_SHORT);
 
                     toast.show();
                     Handler handler = new Handler();
@@ -229,7 +255,7 @@ String newUrl="http://google.co.in/search?q=";
                         }
                     }, DURATION);
                 }   else if ( optionB > optionA && optionB > optionC ) {
-                    final Toast toast=   Toast.makeText(Main2Activity.this, "option  B:  " + optionB + "    "+optionb, Toast.LENGTH_SHORT);
+                    final Toast toast=   Toast.makeText(getActivity(), "option  B:  " + optionB + "    "+optionb, Toast.LENGTH_SHORT);
                     toast.show();
                     Handler handler = new Handler();
                     handler.postDelayed(new Runnable() {
@@ -239,7 +265,7 @@ String newUrl="http://google.co.in/search?q=";
                         }
                     }, DURATION);
                 }    else if ( optionC > optionA && optionC > optionB ) {
-                    final Toast toast=  Toast.makeText(Main2Activity.this, "option    C:  " + optionC + "   "+optionc, Toast.LENGTH_SHORT);
+                    final Toast toast=  Toast.makeText(getActivity(), "option    C:  " + optionC + "   "+optionc, Toast.LENGTH_SHORT);
                     // Log.d("ffdfdfd", "Entered in the webview 2");
                     toast.show();
                     Handler handler = new Handler();
@@ -263,7 +289,7 @@ String newUrl="http://google.co.in/search?q=";
                 }
             }
 
-          if(!(arr.get(i).contains("?")||counter>1))
+            if(!(arr.get(i).contains("?")||counter>1))
             {
                 String arr1[]=arr.get(i).split(" ");
 
@@ -284,11 +310,11 @@ String newUrl="http://google.co.in/search?q=";
             @Override
             public void onFindResultReceived(int i, final int i1, boolean b) {
                 //       btn4.setText(String.valueOf(i1));
-                // Toast.makeText(Main2Activity.this,"OPTION B:"+String.valueOf(i1),Toast.LENGTH_SHORT).show();
+                // Toast.makeText(getActivity().getBaseContext().this,"OPTION B:"+String.valueOf(i1),Toast.LENGTH_SHORT).show();
                 optionB=i1;     //  btn3.setText(String.valueOf(i1));
                 if ( optionA > optionB && optionA > optionC ) {
 
-                    final Toast toast= Toast.makeText(Main2Activity.this, "option   A:  " + optionA + " "+optiona, Toast.LENGTH_SHORT);
+                    final Toast toast= Toast.makeText(getActivity(), "option   A:  " + optionA + " "+optiona, Toast.LENGTH_SHORT);
 
                     toast.show();
                     Handler handler = new Handler();
@@ -299,7 +325,7 @@ String newUrl="http://google.co.in/search?q=";
                         }
                     }, DURATION);
                 }   else if ( optionB > optionA && optionB > optionC ) {
-                    final Toast toast=   Toast.makeText(Main2Activity.this, "option  B:  " + optionB + "    "+optionb, Toast.LENGTH_SHORT);
+                    final Toast toast=   Toast.makeText(getActivity(), "option  B:  " + optionB + "    "+optionb, Toast.LENGTH_SHORT);
                     toast.show();
                     Handler handler = new Handler();
                     handler.postDelayed(new Runnable() {
@@ -309,7 +335,7 @@ String newUrl="http://google.co.in/search?q=";
                         }
                     }, DURATION);
                 }    else if ( optionC > optionA && optionC > optionB ) {
-                    final Toast toast=  Toast.makeText(Main2Activity.this, "option    C:  " + optionC + "   "+optionc, Toast.LENGTH_SHORT);
+                    final Toast toast=  Toast.makeText(getActivity(), "option    C:  " + optionC + "   "+optionc, Toast.LENGTH_SHORT);
                     // Log.d("ffdfdfd", "Entered in the webview 2");
                     toast.show();
                     Handler handler = new Handler();
@@ -358,12 +384,12 @@ String newUrl="http://google.co.in/search?q=";
             @Override
             public void onFindResultReceived(int i, final int i1, boolean b) {
                 //       btn4.setText(String.valueOf(i1));
-                //Toast.makeText(Main2Activity.this,"OPTION C:"+String.valueOf(i1),Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getActivity().getBaseContext().this,"OPTION C:"+String.valueOf(i1),Toast.LENGTH_SHORT).show();
                 //  btn3.setText(Integer.toString(i1));
                 optionC=i1;
                 if ( optionA > optionB && optionA > optionC ) {
 
-                    final Toast toast= Toast.makeText(Main2Activity.this, "option   A:  " + optionA + " "+optiona, Toast.LENGTH_SHORT);
+                    final Toast toast= Toast.makeText(getActivity(), "option   A:  " + optionA + " "+optiona, Toast.LENGTH_SHORT);
 
                     toast.show();
                     Handler handler = new Handler();
@@ -374,7 +400,7 @@ String newUrl="http://google.co.in/search?q=";
                         }
                     }, DURATION);
                 }   else if ( optionB > optionA && optionB > optionC ) {
-                    final Toast toast=   Toast.makeText(Main2Activity.this, "option  B:  " + optionB + "    "+optionb, Toast.LENGTH_SHORT);
+                    final Toast toast=   Toast.makeText(getActivity(), "option  B:  " + optionB + "    "+optionb, Toast.LENGTH_SHORT);
                     toast.show();
                     Handler handler = new Handler();
                     handler.postDelayed(new Runnable() {
@@ -384,7 +410,7 @@ String newUrl="http://google.co.in/search?q=";
                         }
                     }, DURATION);
                 }    else if ( optionC > optionA && optionC > optionB ) {
-                    final Toast toast=  Toast.makeText(Main2Activity.this, "option    C:  " + optionC + "   "+optionc, Toast.LENGTH_SHORT);
+                    final Toast toast=  Toast.makeText(getActivity(), "option    C:  " + optionC + "   "+optionc, Toast.LENGTH_SHORT);
                     // Log.d("ffdfdfd", "Entered in the webview 2");
                     toast.show();
                     Handler handler = new Handler();
@@ -400,7 +426,7 @@ String newUrl="http://google.co.in/search?q=";
         });
 
         webView2.loadUrl(newUrl);
-       // Log.d("ffdfdfd","Load Url");
+        // Log.d("ffdfdfd","Load Url");
         //        webView.loadDataWithBaseURL("x-data://base", newUrl, "text/html", "UTF-8", null);
 
         btnId.setOnClickListener(new View.OnClickListener() {
@@ -415,55 +441,10 @@ String newUrl="http://google.co.in/search?q=";
 
 
 
-    /*    class MyJavaScriptInterface {
 
+        return view;
+    }
+    
 
-            TextView contentVie;
-            @JavascriptInterface
-
-
-
-            public void processHTML(String html) {
-                // process the html as needed by the app
-            }
-
-            public MyJavaScriptInterface(TextView aContentView)
-            {
-                Log.d("sdssdecd","Enter in Constructor");
-                contentVie = aContentView;
-                //   Toast.makeText(Main2Activity.this, (CharSequence) contentView, Toast.LENGTH_SHORT).show();
-            }
-        }
-
-            final WebView browser = (WebView) findViewById(R.id.webView);
-            /* JavaScript must be enabled if you want it to work, obviously */
-   /*     browser.getSettings().
-
-            setJavaScriptEnabled(true);
-
-            /* Register a new JavaScript interface called HTMLOUT */
-     //   browser.addJavascriptInterface(new
-
-       //     MyJavaScriptInterface(contentView), "HTMLOUT");
-
-            /* WebViewClient must be set BEFORE calling loadUrl! */
-        /*browser.setWebViewClient(new
-
-            WebViewClient() {
-                @Override
-                public void onPageFinished (WebView view, String url)
-                {
-                    /* This call inject JavaScript into the page which just finished loading. */
-                    //  browser.loadUrl("javascript:window.HTMLOUT.processHTML('<head>'+document.getElementsByTagName('html')[0].innerHTML+'</head>');");
-                }
-        //    });
-
-
-            /* load a web page */
-      //  browser.loadUrl("http://lexandera.com/files/jsexamples/gethtml.html");
-
-
-
-
-
+   
 }
